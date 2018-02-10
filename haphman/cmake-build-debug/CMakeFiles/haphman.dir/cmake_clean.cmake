b054b172eb047e4ad6ddef40818535255d3c365d
Marat Ziganshin <zig-marat@mail.ru>
2018-02-10 14:43:47 +0300

    haffman

---
+++ haphman/cmake-build-debug/CMakeFiles/haphman.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/haphman.dir/main.cpp.obj"
+  "haphman.pdb"
+  "haphman.exe"
+  "haphman.exe.manifest"
+  "libhaphman.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/haphman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
