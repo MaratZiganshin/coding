b60168afe6f04fdb1282a596202146fb61e88ff4
Marat Ziganshin <zig-marat@mail.ru>
2018-02-21 21:56:10 +0300

    decodeLz77

---
+++ LZ77decode/cmake-build-debug/CMakeFiles/LZ77decode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LZ77decode.dir/main.cpp.obj"
+  "LZ77decode.pdb"
+  "LZ77decode.exe"
+  "LZ77decode.exe.manifest"
+  "libLZ77decode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LZ77decode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LZ77decode/main.cpp
@@ -0,0 +1,40 @@
+﻿#include "ReadWriter.h"
+//Node.h подключен в ReadWriter.h, поэтому здесь не надо
+
+using namespace std;
+
+//Можно добавлять любые вспомогательные методы и классы для решения задачи.
+
+//Задание реализовать метод декодирования
+void decodeLZ77(Node* arr, int n, string& res)
+{
+    for (int i = 0; i < n; i++){
+        Node node = arr[i];
+        if (node.len > 0){
+            int start = res.length() - node.offs;
+            for (int j = 0; j < node.len; j++)
+                res += res[start + j];
+        }
+        res += node.ch;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    ReadWriter rw;
+    
+    int n = rw.readInt();
+    Node* arr = new Node[n];
+
+    rw.readCode(arr, n);
+
+    string res = "";
+    //декодируем в строку
+    decodeLZ77(arr, n, res);
+    //записываем ответ в файл
+    rw.writeString(res);
+
+    delete[] arr;
+
+    return 0;
+}
