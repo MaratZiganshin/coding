d067e363ce1ade9c6b57ed6b4a48b04d07495042
Marat Ziganshin <zig-marat@mail.ru>
2018-03-01 20:33:17 +0300

    wagner-fisher

---
+++ wagner_fisher/cmake-build-debug/CMakeFiles/wagner_fisher.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wagner_fisher.dir/main.cpp.obj"
+  "wagner_fisher.pdb"
+  "wagner_fisher.exe"
+  "wagner_fisher.exe.manifest"
+  "libwagner_fisher.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wagner_fisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
